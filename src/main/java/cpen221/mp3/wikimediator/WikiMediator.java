0f755b426cd228bf18fd2f25b84131b18b5865ff
George Hannam <georgehannam5@gmail.com>
2021-11-27 18:31:16 -0800

    Create task 3 branch

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -10,7 +10,7 @@ public class WikiMediator {
     /* TODO: Implement this datatype
 
         You must implement the methods with the exact signatures
-        as provided in the statement for this mini-project.
+        as provided in the statement for this mini-project
 
         You must add method signatures even for the methods that you
         do not plan to implement. You should provide skeleton implementation
