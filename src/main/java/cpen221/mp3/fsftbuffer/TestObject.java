73e92da94b981f9b10367b9bd421cc7ae2b8f8e7
Julian Lapenna <7jlapenna@gmail.com>
2021-12-02 00:16:29 -0800

    wrote specs for custom classes

--- src/main/java/cpen221/mp3/fsftbuffer/TestObject.java
+++ src/main/java/cpen221/mp3/fsftbuffer/TestObject.java
@@ -3,10 +3,17 @@ package cpen221.mp3.fsftbuffer;
 public class TestObject implements Bufferable {
     private final String id;
 
+    /**
+     * Creates a test object with an identifier
+     * @param id the object's identifier
+     */
     public TestObject(String id) {
         this.id = id;
     }
 
+    /**
+     * @return the object's identifier
+     */
     @Override
     public String id() {
         return id;
