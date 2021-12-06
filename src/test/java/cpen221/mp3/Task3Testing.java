7a1c8c0b1770a1ae3016436fa99bd51602e1c41c
GeorgeDave <georgehannam5@gmail.com>
2021-12-05 22:28:25 -0800

    Writing more test cases

--- src/test/java/cpen221/mp3/Task3Testing.java
+++ src/test/java/cpen221/mp3/Task3Testing.java
@@ -8,12 +8,49 @@ import org.junit.Test;
 
 import java.io.FileWriter;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Task3Testing {
+
+    @Test
+    public void testZeitgeist1() {
+        WikiMediator testingWikiMediator = new WikiMediator(5, 3);
+
+        for (int i = 0; i < 3; i++) {
+            testingWikiMediator.getPage("Dog");
+        }
+        for (int i = 0; i < 10; i++) {
+            testingWikiMediator.search("grapple", 3);
+        }
+        for (int i = 0; i < 5; i++) {
+            testingWikiMediator.getPage("Car");
+        }
+        for (int i = 0; i < 2; i++) {
+            testingWikiMediator.getPage("Hamster");
+            testingWikiMediator.getPage("Cat");
+        }
+
+        Assert.assertEquals(Arrays.asList("grapple", "Car", "Dog"), testingWikiMediator.zeitgeist(3));
+    }
+
+    @Test
+    public void testingSimpleOperations() {
+        WikiMediator testingWikiMediator  =new WikiMediator(10, 90);
+
+        for (int i =0; i < 5; i++) {
+            testingWikiMediator.getPage("Dog");
+        }
+        try{
+            Thread.sleep(50);
+        }
+        catch(Exception e) {
+            System.out.println("error sleeping thread");
+        }
+
+        testingWikiMediator.search("ubama", 5);
+
+
+    }
 
     @Test
     public void testReadingFromJson() {
@@ -48,25 +85,6 @@ public class Task3Testing {
         WikiMediator testingMediator = new WikiMediator(10, 90);
 
         System.out.println(testingMediator.trending(100, 50).get(0));
-
-    }
-
-    @Test
-    public void testingSimpleOperations() {
-        WikiMediator testingWikiMediator  =new WikiMediator(10, 90);
-
-        for (int i =0; i < 5; i++) {
-            testingWikiMediator.getPage("Dog");
-        }
-        try{
-            Thread.sleep(50);
-        }
-        catch(Exception e) {
-            System.out.println("error sleeping thread");
-        }
-
-        testingWikiMediator.search("ubama", 5);
-
 
     }
 }
