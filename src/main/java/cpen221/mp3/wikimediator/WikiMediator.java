f3ec5254da29cc08019a0835983783a4660aaa67
Julian Lapenna <7jlapenna@gmail.com>
2021-12-07 11:51:31 -0800

    Re-added shortest path method

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Comparator;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 /**
@@ -167,6 +168,103 @@ public class WikiMediator {
 
         return result;
     }
+
+    /**
+     *
+     * @param pageTitle1 The title of the source Wikipedia page
+     * @param pageTitle2 The title of the destination Wikipedia page
+     * @param timeout the duration in seconds that the search may last
+     * @return An ordered list of Wikipedia pages that can be traversed by
+     *         internal Wikipedia links to arrive at the destination from
+     *         the source (inclusive). The list provides the shortest possible path
+     *         and in the case of a tie, the lowest lexicographical path
+     * @throws TimeoutException If the operation is not successful in the
+     *                          allotted time an exception will be thrown
+     */
+    public List<String> shortestPath(String pageTitle1, String pageTitle2, int timeout) throws TimeoutException {
+        long endTime = System.currentTimeMillis() + (timeout * 1000L);
+
+        // for the case of the start and end being the same pages
+        // and when there is a page that nothing links to
+        if (pageTitle1.equals(pageTitle2)) return new ArrayList<>(Collections.singleton(pageTitle1));
+        if (wiki.whatLinksHere(pageTitle2).size() == 0) return new ArrayList<>();
+
+        // create initial node with no parent
+        WikiNode startNode = new WikiNode(pageTitle1, null);
+
+        // Arraylist to store the nodes which create the path upon finding the destination
+        ArrayList<WikiNode> queue = new ArrayList<>();
+
+        // Arraylists to store the queue, searched and temp storage of the page links
+        ArrayList<String> queueStrings = new ArrayList<>();
+        ArrayList<String> nodeLinks;
+        ArrayList<String> searchedStrings = new ArrayList<>();
+
+        // Arraylist storing the path that will be returned
+        ArrayList<String> path = new ArrayList<>();
+
+        queue.add(startNode);
+        queueStrings.add(pageTitle1);
+        WikiNode node;
+        String nodeString;
+
+        // add the first node to queue and search
+        for (int i = 0; i < queueStrings.size(); i++) {
+            nodeString = queueStrings.get(i);
+            node = queue.get(i);
+            nodeLinks = wiki.getLinksOnPage(nodeString);
+
+            // if the node's links contain the node we want
+            if (nodeLinks.contains(pageTitle2)) {
+
+                // generate its path and end the search
+                path = getPath(node);
+                break;
+
+                // otherwise, if it also hasn't already been searched
+            } else if (!searchedStrings.contains(nodeString)) {
+
+                // add it to searched
+                searchedStrings.add(nodeString);
+
+                // add its children (in lexicographical order) to the queue
+                queueStrings.addAll(nodeLinks);
+                for (String s : nodeLinks) { // and add its children as nodes to their queue
+                    queue.add(new WikiNode(s, node));
+                }
+            }
+
+            // we don't want to exceed that timeout!
+            if (System.currentTimeMillis() > endTime) {
+                throw new TimeoutException("shortest path search timed-out.");
+            }
+        }
+        // return its path which if no path was found is an empty array list,
+        // and otherwise is the shortest lexicographical path
+        if (path.size() == 0) return path;
+        path.add(pageTitle2);
+        return path;
+    }
+
+    /**
+     * Given a child WikiNode, traverse up the tree of its parents to
+     * find a path from the upper-most parent to the given child WikiNode
+     * @param w the child WikiNode
+     * @return a list of IDs including the parent and child nodes that
+     *         represent a path that can be taken from the parent Wikipedia
+     *         page to arrive at the child Wikipedia page
+     */
+    private ArrayList<String> getPath(WikiNode w) {
+        ArrayList<String> path = new ArrayList<>();
+
+        if (w.getParent() != null) {
+            path.addAll(getPath(w.getParent()));
+        }
+
+        path.add(w.getId());
+        return path;
+    }
+
 
     /**
      * Returns the most common search queries and getPage requests as a sorted list since the start of Wikimediator
